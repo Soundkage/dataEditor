a33096775914e6f8bdcba3b197d23d8887093827
Dean Thomson <dean.thomson@derivco.co.za>
2021-07-12 17:38:36 +0200

    Added export feature

--- src/components/export-button/export-button.vue
+++ src/components/export-button/export-button.vue
@@ -1,23 +1,34 @@
 <template>
   <div class="export">
-    <button
-      class="button button--green button__export"
-      @click="exportList(data.value)"
-    >
+    <button class="button button--green button__export" @click="exportList()">
       Export List
     </button>
   </div>
 </template>
 
 <script>
+import store from '../../store';
+
 export default {
   name: 'ExportButton',
   props: {
     data: Object
   },
   methods: {
-    exportList: data => {
-      console.log('>>>> Export', data);
+    exportList: () => {
+      const exportData = store.getters.getProfileData || null;
+      if (exportData) {
+        const dataBlob = new Blob([JSON.stringify(exportData, null, 2)], {
+          type: 'application/json'
+        });
+
+        const url = URL.createObjectURL(dataBlob);
+        const dummyLink = document.createElement('a');
+
+        dummyLink.href = url;
+        dummyLink.download = `profileList-${Date.now()}.json`;
+        dummyLink.click();
+      }
     }
   }
 };
