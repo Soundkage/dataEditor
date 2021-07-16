d66e9bd8185916c6e2397a0e595e616571ccda6d
Dean Thomson <dean.thomson@derivco.co.za>
2021-07-16 19:54:33 +0200

    Begun setup of unit tests

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: '@vue/cli-plugin-unit-jest',
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.*\\.(js)$': 'babel-jest'
+  },
+  collectCoverage: true,
+  coverageDirectory: './coverage',
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageReporters: ['lcovonly', 'text'],
+  testMatch: ['**/*Spec.ts']
+};
---
+++ src/components/header/headerSpec.ts
@@ -0,0 +1,7 @@
+// import Header from './header.vue';
+
+describe('Test', () => {
+  it('should do something', () => {
+    expect(true).toBeTruthy();
+  });
+});
